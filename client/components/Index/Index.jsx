2f9c621e4de09026a9a20bd9619f178d858def5f
Forrest Desjardins <desjardinsfg@gmail.com>
2017-07-16 13:38:54 -0800

    stub out more sections and examples

--- client/components/Index/Index.jsx
+++ client/components/Index/Index.jsx
@@ -4,9 +4,37 @@ import { Link } from 'inferno-router'
 const Index = () => {
   return (
     <ul>
-      <li><Link to='/01'>01_hello-world</Link></li>
-      <li><Link to='/02'>02_hello-2d-world</Link></li>
-      <li><Link to='/03'>03_hello-3d-world</Link></li>
+      <li>
+        <h2>Basics</h2>
+        <ul>
+          <li><Link to='/01'>hello world</Link></li>
+          <li><Link to='/02'>hello 2d world</Link></li>
+          <li><Link to='/03'>hello 3d world</Link></li>
+          <li>lighting</li>
+          <li>objects and textures</li>
+          <li>custom shaders</li>
+          <li>scene graphs</li>
+        </ul>
+      </li>
+      <li>
+        <h2>Advanced WebGL</h2>
+        <ul>
+          <li>shadows</li>
+          <li>stencil testing</li>
+          <li>blending</li>
+          <li>framebuffers</li>
+          <li>cubemaps</li>
+          <li>geometry shaders</li>
+        </ul>
+      </li>
+      <li>
+        <h2>Using gpu.js</h2>
+        <ul>
+          <li>matrix multiplication</li>
+          <li>...</li>
+          <li>n-body sim</li>
+        </ul>
+      </li>
     </ul>
   )
 }
