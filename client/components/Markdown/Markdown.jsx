33a2b5cd82d099def5250d224a59201838957a34
Forrest Desjardins <desjardinsfg@gmail.com>
2017-07-16 23:37:35 -0800

    add math rendering to markdown

--- client/components/Markdown/Markdown.jsx
+++ client/components/Markdown/Markdown.jsx
@@ -1,8 +1,10 @@
 import Inferno from 'inferno'
 import MarkdownIt from 'markdown-it'
 import dompurify from 'dompurify'
+import markdownItLatex from 'markdown-it-latex'
 
 const md = new MarkdownIt()
+md.use(markdownItLatex)
 
 export default ({ text }) => {
   return <div class='notes' dangerouslySetInnerHTML={ { __html: dompurify.sanitize(md.render(text)) } } />
