acfe454b35e66443767b51dfebb3bdf3f6eed970
Forrest Desjardins <desjardinsfg@gmail.com>
2017-07-16 16:12:04 -0800

    eslint fixes

--- client/components/Threejs/Threejs01/Threejs01.jsx
+++ client/components/Threejs/Threejs01/Threejs01.jsx
@@ -1,36 +1,34 @@
 import Inferno from 'inferno'
- import { default as utils, sq } from '-/utils'
+import { default as utils, sq } from '-/utils'
 import './Threejs01.scss'
 
+const init = ({}) => {
+  const scene = new THREE.Scene()
+  const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
 
-const init =({})=>{
-  var scene = new THREE.Scene();
-  var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
+  const renderer = new THREE.WebGLRenderer()
+  renderer.setSize(window.innerWidth, window.innerHeight)
+  document.body.appendChild(renderer.domElement)
 
-  var renderer = new THREE.WebGLRenderer();
-  renderer.setSize( window.innerWidth, window.innerHeight );
-  document.body.appendChild( renderer.domElement );
+  const geometry = new THREE.BoxGeometry(1, 1, 1)
+  const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 })
+  const cube = new THREE.Mesh(geometry, material)
+  scene.add(cube)
 
-  var geometry = new THREE.BoxGeometry( 1, 1, 1 );
-  var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-  var cube = new THREE.Mesh( geometry, material );
-  scene.add( cube );
-
-  camera.position.z = 5;
+  camera.position.z = 5
 }
 
-const Threejs01 = ({ init}) => {
+const Threejs01 = ({ init }) => {
+  const animate = function () {
+    requestAnimationFrame(animate)
 
-  var animate = function () {
-    requestAnimationFrame( animate );
+    cube.rotation.x += 0.1
+    cube.rotation.y += 0.1
 
-    cube.rotation.x += 0.1;
-    cube.rotation.y += 0.1;
+    renderer.render(scene, camera)
+  }
 
-    renderer.render(scene, camera);
-  };
-
-  return(
+  return (
 
     <h1> Hello World </h1>
 
