c5cdca7c0833068d3193b5a9ee6be562fe81b4b6
Forrest Desjardins <desjardinsfg@gmail.com>
2017-07-16 16:38:48 -0800

    WIP

--- client/components/Threejs/Threejs01/Threejs01.jsx
+++ client/components/Threejs/Threejs01/Threejs01.jsx
@@ -1,25 +1,22 @@
 import Inferno from 'inferno'
 import { default as utils, sq } from '-/utils'
 import './Threejs01.scss'
+// import THREE from 'threejs'
 
-const init = ({}) => {
+const didMount = (subscribe) => {
+  console.log('didMount')
   const scene = new THREE.Scene()
   const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
-
-  const renderer = new THREE.WebGLRenderer()
-  renderer.setSize(window.innerWidth, window.innerHeight)
-  document.body.appendChild(renderer.domElement)
-
-  const geometry = new THREE.BoxGeometry(1, 1, 1)
-  const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 })
-  const cube = new THREE.Mesh(geometry, material)
-  scene.add(cube)
-
   camera.position.z = 5
-}
 
-const Threejs01 = ({ init }) => {
-  const animate = function () {
+  console.log(document.querySelector('#threejs'))
+  const renderer = new THREE.WebGLRenderer({ canvas: document.querySelector('#threejs') })
+  renderer.setSize(window.innerWidth, window.innerHeight)
+
+  const cube = new THREE.Mesh(geometry, material)
+
+  const animate = () => {
+    console.log('animate')
     requestAnimationFrame(animate)
 
     cube.rotation.x += 0.1
@@ -28,12 +25,37 @@ const Threejs01 = ({ init }) => {
     renderer.render(scene, camera)
   }
 
+  const geometry = new THREE.BoxGeometry(1, 1, 1)
+  const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 })
+
+  scene.add(cube)
+
+  // window.renderer = renderer
+
+  // renderer.domElement = document.querySelector('#threejs')
+  // document.getElementById('threejs').innerHTML =
+
+  animate()
+}
+
+const Threejs = () => {
   return (
-
-    <h1> Hello World </h1>
-
+    <canvas id='threejs'></canvas>
   )
 }
+
+const Threejs01 = ({ subscribe }) => {
+  console.log('render')
+  return (
+    <div id="threejs01">
+      <h1> Hello World </h1>
+      <Threejs
+        onComponentDidMount={ didMount(subscribe) }
+      />
+    </div>
+  )
+}
+
 export default ({ children }, { store }) => {
   const subscribe = callback => {
     store.select(sq('Threejs01.scene')).on('update', ({ data }) => callback(data.currentData))
@@ -41,6 +63,7 @@ export default ({ children }, { store }) => {
   return (
     <Threejs01
       subscribe={ subscribe }
+      onComponentShouldUpdate={ utils.shouldUpdate }
     />
   )
 }
