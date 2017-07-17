b075dc39cf15a33a2e1985e26b064f2705f8779d
Forrest Desjardins <desjardinsfg@gmail.com>
2017-07-16 16:49:12 -0800

    WIP

--- client/components/Threejs/Threejs01/Threejs01.jsx
+++ client/components/Threejs/Threejs01/Threejs01.jsx
@@ -6,51 +6,46 @@ import './Threejs01.scss'
 const didMount = (subscribe) => {
   console.log('didMount')
   const scene = new THREE.Scene()
-  const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
+  const camera = new THREE.PerspectiveCamera(75, 400 / 400, 0.1, 1000)
   camera.position.z = 5
 
-  console.log(document.querySelector('#threejs'))
-  const renderer = new THREE.WebGLRenderer({ canvas: document.querySelector('#threejs') })
-  renderer.setSize(window.innerWidth, window.innerHeight)
+  const renderer = new THREE.WebGLRenderer()
+  renderer.setSize(400, 400)
 
   const cube = new THREE.Mesh(geometry, material)
-
-  const animate = () => {
-    console.log('animate')
-    requestAnimationFrame(animate)
-
-    cube.rotation.x += 0.1
-    cube.rotation.y += 0.1
-
-    renderer.render(scene, camera)
-  }
 
   const geometry = new THREE.BoxGeometry(1, 1, 1)
   const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 })
 
   scene.add(cube)
 
-  // window.renderer = renderer
+  const animate = () => {
+    requestAnimationFrame(animate)
 
-  // renderer.domElement = document.querySelector('#threejs')
-  // document.getElementById('threejs').innerHTML =
+    cube.rotation.x += 0.1
+    cube.rotation.y += 0.1
+    console.log('animate')
+
+    renderer.render(scene, camera)
+  }
+
+  document.querySelector('#threejs').appendChild(renderer.domElement)
 
   animate()
 }
 
 const Threejs = () => {
   return (
-    <canvas id='threejs'></canvas>
+    <div id='threejs'></div>
   )
 }
 
 const Threejs01 = ({ subscribe }) => {
-  console.log('render')
   return (
     <div id="threejs01">
       <h1> Hello World </h1>
       <Threejs
-        onComponentDidMount={ didMount(subscribe) }
+        onComponentDidMount={ () => didMount(subscribe) }
       />
     </div>
   )
