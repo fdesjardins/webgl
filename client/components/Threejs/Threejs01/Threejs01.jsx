1360051642c8bab9be2e8744e28fa87f07972612
ubernaut <collin.schroeder@gmail.com>
2017-07-16 17:04:16 -0800

    dynamic size

--- client/components/Threejs/Threejs01/Threejs01.jsx
+++ client/components/Threejs/Threejs01/Threejs01.jsx
@@ -6,12 +6,12 @@ const didMount = (subscribe) => {
   console.log('didMount')
   const scene = new THREE.Scene()
 
-  const camera = new THREE.PerspectiveCamera(75, 400 / 400, 0.1, 1000)
+  const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)//new THREE.PerspectiveCamera(75, 400 / 400, 0.1, 1000)
   camera.position.z = 5
 
   const renderer = new THREE.WebGLRenderer({ canvas: document.querySelector('#threejs') })
-  renderer.setSize(400, 400)
-
+  //renderer.setSize(400, 400)
+  renderer.setSize(window.innerWidth, window.innerHeight)
   const geometry = new THREE.BoxGeometry(1, 1, 1)
   const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 })
   const cube = new THREE.Mesh(geometry, material)
