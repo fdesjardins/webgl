74d7f7711df172f9be9c8b846a65665f59b890a7
Forrest Desjardins <desjardinsfg@gmail.com>
2019-11-11 13:45:05 -0500

    Update server for heroku

--- server/server.js
+++ server/server.js
@@ -1,38 +1,16 @@
-const fs = require('fs')
 const path = require('path')
 const express = require('express')
 const compression = require('compression')
 const cors = require('cors')
-// const browserSync = require('browser-sync')
 
-module.exports = async (config) => {
-  const app = express()
-  app.use(compression())
-  app.use('/dist', express.static(path.join(__dirname, '../dist')))
-  app.use('/lib', express.static(path.join(__dirname, '../lib')))
+const port = process.env.PORT || 9000
 
-  app.use(cors())
-  app.all('*', cors())
+const app = express()
+app.use(compression())
+app.use(express.static(path.join(__dirname, '../dist')))
+app.use('/lib', express.static(path.join(__dirname, '../lib')))
 
-  const layout = (await fs.readFileSync(path.join(__dirname, 'layout.html'))).toString()
-  app.get('*', (req, res, next) => {
-    res.send(layout)
-  })
+app.use(cors())
+app.all('*', cors())
 
-  const server = app.listen(1137)
-  console.log('listening on 1137...')
-
-  // const bs = browserSync.create()
-  // bs.init({
-  //   proxy: 'localhost:1137',
-  //   reloadDelay: 100
-  // })
-  // bs.watch(path.join(__dirname, '../dist/**/*.js')).on('change', bs.reload)
-  // bs.watch(path.join(__dirname, '../dist/**/*.css')).on('change', bs.reload)
-
-  return { app, server }
-}
-
-if (!module.parent) {
-  module.exports({})
-}
+app.listen(port, () => console.log(`Listening on port ${port}`))
