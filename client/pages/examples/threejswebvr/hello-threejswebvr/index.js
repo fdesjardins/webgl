f980bd441ea40b897eed89298d0c217977c5af46
ubernaut <collin.schroeder@gmail.com>
2019-11-28 12:40:02 -0900

    added host: 0000

--- client/pages/examples/threejswebvr/hello-threejswebvr/index.js
+++ client/pages/examples/threejswebvr/hello-threejswebvr/index.js
@@ -1,9 +1,7 @@
-import  'three/examples/js/vr/HelioWebXRPolyfill.js'
-
 import React from 'react'
 import ReactDOM from 'react'
 import * as THREE from 'three'
-
+import  'three/examples/js/vr/HelioWebXRPolyfill.js'
 import Example from '-/components/example'
 import notes from './readme.md'
 import { VRButton } from 'three/examples/jsm/webxr/VRButton.js'
