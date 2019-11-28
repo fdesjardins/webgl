3a239aee33ded890381740508b679dc9b4af699e
ubernaut <collin.schroeder@gmail.com>
2019-11-28 12:12:57 -0900

    removed jsm crap and switched to webxr

--- client/pages/examples/threejswebvr/hello-threejswebvr/index.js
+++ client/pages/examples/threejswebvr/hello-threejswebvr/index.js
@@ -1,12 +1,12 @@
+import  'three/examples/js/vr/HelioWebXRPolyfill.js'
+
 import React from 'react'
 import ReactDOM from 'react'
 import * as THREE from 'three'
 
 import Example from '-/components/example'
 import notes from './readme.md'
-import { WEBVR } from 'three/examples/jsm/vr/WebVR.js'
-
-console.log(WEBVR)
+import { VRButton } from 'three/examples/jsm/webxr/VRButton.js'
 
 let button
 let renderer
@@ -29,35 +29,36 @@ const didMount = ({ canvas, container }) => {
 
   renderer = new THREE.WebGLRenderer({ canvas })
   renderer.vr.enabled = true
-  button = WEBVR.createButton(renderer)
+  button = VRButton.createButton(renderer)
   document.getElementById('webvr-button').appendChild(button)
 
   renderer.setSize(container.clientWidth, container.clientWidth)
   const geometry = new THREE.BoxGeometry(1, 1, 1)
   const material = new THREE.MeshPhongMaterial({ color: 0x666666 })
+  //const material = new THREE.MeshBasicMaterial({ color: 0x666666 })
   const cube = new THREE.Mesh(geometry, material)
   cube.matrixAutoUpdate = true
-  cube.castShadow = true
+  //cube.castShadow = true
   scene.add(cube)
 
-  const planeGeometry = new THREE.PlaneBufferGeometry(100, 100, 100, 100)
-  const planeMaterial = new THREE.MeshStandardMaterial({ color: 0xffffff })
-  const plane = new THREE.Mesh(planeGeometry, planeMaterial)
-  plane.position.z = -2
-  plane.position.y = 0
-  plane.position.x = 0
-  plane.rotation.x = 0
-  plane.rotation.y = 0
-  plane.receiveShadow = true
-  scene.add(plane)
+  // const planeGeometry = new THREE.PlaneBufferGeometry(100, 100, 100, 100)
+  // const planeMaterial = new THREE.MeshStandardMaterial({ color: 0xffffff })
+  // const plane = new THREE.Mesh(planeGeometry, planeMaterial)
+  // plane.position.z = -2
+  // plane.position.y = 0
+  // plane.position.x = 0
+  // plane.rotation.x = 0
+  // plane.rotation.y = 0
+  // plane.receiveShadow = true
+  // scene.add(plane)
 
   const light = new THREE.PointLight(0xffffff, 1, 100)
   light.position.set(0, 0, 12)
-  light.castShadow = true
-  light.shadow.mapSize.width = 1024
-  light.shadow.mapSize.height = 1024
-  light.shadow.camera.near = 0.5
-  light.shadow.camera.far = 500
+  // light.castShadow = true
+  // light.shadow.mapSize.width = 1024
+  // light.shadow.mapSize.height = 1024
+  // light.shadow.camera.near = 0.5
+  // light.shadow.camera.far = 500
   scene.add(light)
 
   const animate = () => {
@@ -78,12 +79,12 @@ const didMount = ({ canvas, container }) => {
 
 const update = () =>
   didMount({
-    canvas: document.querySelector('#threejs01 canvas'),
-    container: document.querySelector('#threejs01')
+    canvas: document.querySelector('#threejsvr01 canvas'),
+    container: document.querySelector('#threejsvr01')
   })
 
 const HelloWebVr = ({ children }, { store }) => (
-  <div id="threejs01">
+  <div id="threejsvr01">
     <span id="webvr-button" />
     <Example notes={notes} didMount={update} didUpdate={update} />
   </div>
