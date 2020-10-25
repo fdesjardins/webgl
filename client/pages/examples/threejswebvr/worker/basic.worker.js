55a395882c10b6751c6bd0395a695b8fe47fec81
cos <collin.schroeder@gmail.com>
2020-10-24 20:33:25 -0800

    gave each worker a random unique speed.

--- client/pages/examples/threejswebvr/worker/basic.worker.js
+++ client/pages/examples/threejswebvr/worker/basic.worker.js
@@ -1,6 +1,7 @@
+const workerSpeed = 3*Math.random()
 onmessage = function(e) {
   postMessage('received');
-  const result = [.5-Math.random(),.5-Math.random(),.5-Math.random()]
+  const result = [(.5-Math.random())*workerSpeed,(.5-Math.random())*workerSpeed,(.5-Math.random())*workerSpeed]
   if (false) {
 
   } else {
