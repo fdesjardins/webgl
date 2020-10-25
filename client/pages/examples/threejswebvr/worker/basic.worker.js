d0a8bba0feaaf8bea9e0c1cd79e19a3108c53829
cos <collin.schroeder@gmail.com>
2020-10-24 22:01:55 -0800

    gave workers more personality

--- client/pages/examples/threejswebvr/worker/basic.worker.js
+++ client/pages/examples/threejswebvr/worker/basic.worker.js
@@ -1,4 +1,4 @@
-const workerSpeed = 3*Math.random()
+const workerSpeed = 3*Math.random() +0.2
 onmessage = function(e) {
   postMessage('received');
   const result = [(.5-Math.random())*workerSpeed,(.5-Math.random())*workerSpeed,(.5-Math.random())*workerSpeed]
