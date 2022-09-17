7d9b0b6588375d3f507db569e766abd08692fa90
ubernaut <collin.schroeder@gmail.com>
2022-09-17 14:38:42 -0800

    added new broken example

---
+++ src/pages/load-model-threejs/LoadModelThree.js
@@ -0,0 +1,68 @@
+import * as THREE from 'three'
+import Stats from 'stats.js'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+
+import { onResize } from '../../utils'
+
+export const init = ({ canvas, container }) => {
+  const scene = new THREE.Scene()
+  scene.background = new THREE.Color(0xFFFFFF)
+
+  const stats = new Stats()
+  stats.showPanel(0)
+  container.appendChild(stats.dom)
+  stats.dom.className = 'stats'
+
+  const camera = new THREE.PerspectiveCamera(
+    75,
+    canvas.clientWidth / canvas.clientHeight,
+    0.1,
+    2000
+  )
+  camera.updateProjectionMatrix()
+  camera.position.set(2, 1, 2)
+
+  const controls = new OrbitControls(camera, canvas)
+  controls.update()
+
+  let renderer = new THREE.WebGLRenderer({ canvas, antialias: true })
+  renderer.setSize(canvas.clientWidth, canvas.clientHeight)
+
+  const handleResize = (event) => {
+    event.preventDefault()
+    onResize({ canvas, camera, renderer })
+  }
+  window.addEventListener('resize', handleResize, false)
+  onResize({ canvas, camera, renderer })
+
+  const geometry = new THREE.BoxGeometry(1, 1, 1)
+  const material = new THREE.MeshLambertMaterial({ color: 0xff0000 })
+  const cube = new THREE.Mesh(geometry, material)
+  scene.add(cube)
+
+  const light = new THREE.PointLight(0xffffff, 1, 100)
+  light.position.set(0, 3, 5)
+  scene.add(light)
+
+  const clock = new THREE.Clock()
+  const animate = () => {
+    if (!renderer) {
+      return
+    }
+    stats.begin()
+    requestAnimationFrame(animate)
+    renderer.render(scene, camera)
+    cube.rotateY(clock.getDelta() * 0.5)
+    stats.end()
+  }
+  animate()
+
+  return () => {
+    renderer.dispose()
+    stats.scene = null
+    container.removeChild(stats.dom)
+    renderer = null
+    window.removeEventListener('resize', handleResize)
+    controls.dispose()
+  }
+}
