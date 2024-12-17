573de288db8a8ef5c6753a9c9ce6bbf73e4c23ac
Forrest Desjardins <desjardinsfg@gmail.com>
2024-12-17 02:15:49 +0000

    0eb4a5d

---
+++ worker.ee7af9a7.js
@@ -0,0 +1,2 @@
+const workerSpeed=3*Math.random()+.2;onmessage=function(e){postMessage("received");const o=[(.5-Math.random())*workerSpeed,(.5-Math.random())*workerSpeed,(.5-Math.random())*workerSpeed];postMessage(o)};
+//# sourceMappingURL=worker.ee7af9a7.js.map
