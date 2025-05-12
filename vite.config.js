6d00b3aaf3c3279fecf41ffe073cb7633f3a69da
Forrest Desjardins <desjardinsfg@gmail.com>
2025-05-12 13:59:05 -0600

    Switch to vite

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+import glsl from 'vite-plugin-glsl'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [glsl(), react()],
+})
